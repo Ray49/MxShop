013a0df9748f50e121b921014765ad437a8dd318
Ray49 <39585181+Ray49@users.noreply.github.com>
2018-10-24 10:59:48 +0800

    Update const.py

--- MxShop/dysms_python/const.py
+++ MxShop/dysms_python/const.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
-ACCESS_KEY_ID = "LTAIhqfe022vh6cq"
-ACCESS_KEY_SECRET = "sSp7qeuLB533kkzWRK8v0pMvvT7vtA"
+ACCESS_KEY_ID = ""
+ACCESS_KEY_SECRET = ""
 #LTAIhqfe022vh6cq
